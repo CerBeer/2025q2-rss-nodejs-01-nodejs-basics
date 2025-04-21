6a8f2182c0d60a40e26992597ac179df6f69508f
CerBeer <ocelopp@gmail.com>
2025-04-21 19:53:29 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import path from "path";
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+
+  const targetPath = path.join(currentPath, "files/");
+  const targetFileName = "fileToRemove.txt";
+  const targetFilePath = path.join(targetPath, targetFileName);
+
+  // Check if the file already exists
+  const exists = await fs
+    .stat(targetFilePath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!exists) {
+    throw new Error("FS operation failed");
+  }
+
+  await fs.unlink(targetFilePath);
 };
 
 await remove();
