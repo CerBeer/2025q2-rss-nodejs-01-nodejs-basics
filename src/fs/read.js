3705ebb48a2e08085539ac95ac808d18f7b4b595
CerBeer <ocelopp@gmail.com>
2025-04-21 20:31:34 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import path from "path";
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+
+  const targetPath = path.join(currentPath, "files/");
+  const targetFileName = "fileToRead.txt";
+  const targetFilePath = path.join(targetPath, targetFileName);
+
+  // Check if the file already exists
+  const exists = await fs
+    .stat(targetFilePath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!exists) {
+    throw new Error("FS operation failed");
+  }
+
+  const text = await fs.readFile(targetFilePath, {
+    encoding: "utf-8",
+  });
+  console.log(text);
 };
 
 await read();
