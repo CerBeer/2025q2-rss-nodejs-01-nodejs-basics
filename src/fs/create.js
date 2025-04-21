fcf8fc7b89a31c946f2a4ab38876eeadbac61b10
CerBeer <ocelopp@gmail.com>
2025-04-21 19:49:53 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,28 +1,27 @@
-import path from 'path';
-import fs from 'fs/promises';
+import path from "path";
+import fs from "fs/promises";
 
 const create = async () => {
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
 
-const args = process.argv;
-const __filename = args[1];
-const currentPath = path.dirname(__filename);
+  const targetPath = path.join(currentPath, "files/");
+  const targetFileName = "fresh.txt";
+  const targetFileContent = "I am fresh and young";
+  const targetFilePath = path.join(targetPath, targetFileName);
 
-const targetPath = path.join(currentPath, 'files/');
-const targetFileName = 'fresh.txt';
-const targetFileContent = 'I am fresh and young';
-const targetFilePath = path.join(targetPath, targetFileName);
-
-// Check if the file already exists
-const exists = await fs.stat(targetFilePath)
-   .then(() => true)
-   .catch(() => false);
+  // Check if the file already exists
+  const exists = await fs
+    .stat(targetFilePath)
+    .then(() => true)
+    .catch(() => false);
 
   if (exists) {
-    throw new Error('FS operation failed');
+    throw new Error("FS operation failed");
   }
-  
-  await fs.writeFile(targetFilePath, targetFileContent);
 
+  await fs.writeFile(targetFilePath, targetFileContent);
 };
 
 await create();
