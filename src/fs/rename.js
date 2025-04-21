d531469f1187e80f8ebfa20aa69fcc11b1367719
CerBeer <ocelopp@gmail.com>
2025-04-21 20:38:54 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,35 @@
+import path from "path";
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+
+  const targetPath = path.join(currentPath, "files/");
+  const sourceFileName = "wrongFilename.txt";
+  const sourceFilePath = path.join(targetPath, sourceFileName);
+  const targetFileName = "properFilename.md";
+  const targetFilePath = path.join(targetPath, targetFileName);
+  
+  // Check if the file already exists
+  const existsSource = await fs
+    .stat(sourceFilePath)
+    .then(() => true)
+    .catch(() => false);
+
+    const existsTarget = await fs
+    .stat(targetFilePath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (existsTarget || !existsSource) {
+    throw new Error("FS operation failed");
+  }
+
+  // Rename the file
+  await fs.rename(sourceFilePath, targetFilePath);
+
 };
 
 await rename();
