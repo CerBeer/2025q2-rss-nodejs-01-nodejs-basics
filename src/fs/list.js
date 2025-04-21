02ad1019b76a6b7617dbe37a64a02d4433a7f099
CerBeer <ocelopp@gmail.com>
2025-04-21 20:14:36 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import path from "path";
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+
+  const targetPath = path.join(currentPath, "files/");
+
+  // Check if the file already exists
+  const exists = await fs
+    .stat(targetPath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!exists) {
+    throw new Error("FS operation failed");
+  }
+
+  const files = await fs.readdir(targetPath, { withFileTypes: true });
+  console.log(files
+    .filter((file) => file.isFile())
+    .map((file) => file.name));
 };
 
 await list();
