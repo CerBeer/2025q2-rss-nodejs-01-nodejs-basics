41c3874cf478b1cb23af37e6fe2fb0709736872e
CerBeer <ocelopp@gmail.com>
2025-04-21 21:02:03 +0300

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import path from "path";
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+
+  const sourceFolderName = "files";
+  const sourceFolderPath = path.join(currentPath, sourceFolderName);
+  const targetFolderName = "files_copy";
+  const targetFolderPath = path.join(currentPath, targetFolderName);
+  
+  // Check if the file already exists
+  const existsSource = await fs
+    .stat(sourceFolderPath)
+    .then(() => true)
+    .catch(() => false);
+
+    const existsTarget = await fs
+    .stat(targetFolderPath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (existsTarget || !existsSource) {
+    throw new Error("FS operation failed");
+  }
+
+  await fs.mkdir(targetFolderPath, { recursive: true });
+  await fs.cp(sourceFolderPath, targetFolderPath, {
+    recursive: true,
+    force: true,
+  });
 };
 
 await copy();
