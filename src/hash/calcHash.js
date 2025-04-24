e28592fb8e1f1a506218c6be1a3a26f1e536d07f
CerBeer <ocelopp@gmail.com>
2025-04-24 19:50:48 +0300

    feat: implement cli

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = path.dirname(__filename);
+  const targetPath = path.join(currentPath, 'files/');
+  const targetFileName = 'fileToCalculateHashFor.txt';
+  const targetFilePath = path.join(targetPath, targetFileName);  
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(targetFilePath);
+
+  stream.on('data', (chunk) => {
+      hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+      console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
