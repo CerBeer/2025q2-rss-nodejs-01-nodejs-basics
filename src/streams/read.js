06379c99cbdbbe3908ea180a4edff80e0a792dee
CerBeer <ocelopp@gmail.com>
2025-04-24 20:14:38 +0300

    feat: implement stream read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const args = process.argv;
+  const __filename = args[1];
+  const currentPath = dirname(__filename);
+  const filePath = join(currentPath, 'files', 'fileToRead.txt');
+  const stream = createReadStream(filePath, { encoding: 'utf-8' });
+
+  stream.on("data", (chunk) => process.stdout.write(chunk.toString()));
+  stream.on("end", () => process.stdout.write("\n"));
+
 };
 
 await read();
